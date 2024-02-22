33d13f2b01d7d53def1d7d22ebae048266835668
Elson Otake <elsonotake@yahoo.com.br>
2024-02-22 16:17:28 -0300

    Create list of boletos

--- app/controllers/boletos_controller.rb
+++ app/controllers/boletos_controller.rb
@@ -1,5 +1,13 @@
 class BoletosController < ApplicationController
+  Required = %i(amount expire_at customer_person_name customer_cnpj_cpf customer_state customer_city_name 
+                customer_zipcode customer_address customer_neighborhood)
   def index
+    # Listar os boletos
+    @billets = []
+    bank_billets = BoletoSimples::BankBillet.all(page: 1, per_page: 50)
+    bank_billets.each do |bank_billet|
+      @billets << bank_billet.attributes.slice(*Required)
+    end
   end
 
   def show
