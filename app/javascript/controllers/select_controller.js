33c14351c49cf7b563f5244e88652b08ab5185db
Elson Otake <elsonotake@yahoo.com.br>
2024-02-23 10:24:05 -0300

    Create js to update list of cities

---
+++ app/javascript/controllers/select_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "state", "city" ];
+  static values = { url: String }
+
+  load_option = (tag, values) => {
+    values.forEach((value) => {
+      const option = document.createElement("option");
+      option.value = value;
+      option.text = value;
+      tag.appendChild(option);
+    });
+  }
+
+  state_change = () => {
+    console.log("state_change");
+
+    if (this.stateTarget.value != "") {
+      this.cityTarget.length = 0;
+      const url = new URL(this.urlValue)
+      url.searchParams.append("state", this.stateTarget.value)
+      fetch(url.href)
+      .then(response => response.json())
+      .then((data) => this.load_option(this.cityTarget, data))
+      .catch(() => [])
+    }
+  }
+}
