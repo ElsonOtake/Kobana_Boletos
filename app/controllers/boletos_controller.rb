09e2561f2bcef774deba21a1de55af28504f5519
Elson Otake <elsonotake@yahoo.com.br>
2024-02-23 11:59:24 -0300

    Create new boleto

--- app/controllers/boletos_controller.rb
+++ app/controllers/boletos_controller.rb
@@ -10,18 +10,35 @@ class BoletosController < ApplicationController
     end
   end
 
+  def new
+
+  end
+
   def show
   end
 
   def create
-    @states = CS.states(:BR).keys
+    @bank_billet = BoletoSimples::BankBillet.new()
+    @bank_billet.amount = params[:boleto][:amount]
+    @bank_billet.expire_at = params[:boleto][:expire_at]
+    @bank_billet.customer_address = params[:boleto][:customer_address]
+    @bank_billet.customer_city_name = params[:boleto][:customer_city_name]
+    @bank_billet.customer_cnpj_cpf = params[:boleto][:customer_cnpj_cpf]
+    @bank_billet.customer_neighborhood = params[:boleto][:customer_neighborhood]
+    @bank_billet.customer_person_name = params[:boleto][:customer_person_name]
+    @bank_billet.customer_state = params[:boleto][:customer_state]
+    @bank_billet.customer_zipcode = params[:boleto][:customer_zipcode]
+    @bank_billet.save
+
+    if @bank_billet.persisted?
+      redirect_to root_path
+    else
+      puts "Erro :(#{@bank_billet.response_errors})"
+      redirect_to root_path
+    end
   end
 
   def cancel
   end
 
-  def cities
-    cities = CS.cities(params[:state], :BR)
-    render json: cities
-  end
 end
