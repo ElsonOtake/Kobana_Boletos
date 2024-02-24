50e48bb3c5d1352e783fb5212ec8a296b0c7bb65
Elson Otake <elsonotake@yahoo.com.br>
2024-02-24 09:39:52 -0300

    Fix unused path

--- test/controllers/boletos_controller_test.rb
+++ test/controllers/boletos_controller_test.rb
@@ -2,22 +2,12 @@ require "test_helper"
 
 class BoletosControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get boletos_index_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get boletos_show_url
+    get boletos_url
     assert_response :success
   end
 
   test "should get create" do
-    get boletos_create_url
-    assert_response :success
-  end
-
-  test "should get cancel" do
-    get boletos_cancel_url
+    get new_boleto_url
     assert_response :success
   end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get cities" do
-    get home_cities_url
+    get cities_url
     assert_response :success
   end
 end
