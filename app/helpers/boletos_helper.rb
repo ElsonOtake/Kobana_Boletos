c42c2d8176f217602254342fa93569a01d7d51c1
Elson Otake <elsonotake@yahoo.com.br>
2024-02-24 16:35:46 -0300

    Refactor show boleto

--- app/helpers/boletos_helper.rb
+++ app/helpers/boletos_helper.rb
@@ -1,2 +1,14 @@
 module BoletosHelper
+  def display(field, value)
+    case field
+    when "amount"
+      number_to_currency(value.to_f, unit: "")
+    when "customer_zipcode"
+      "#{value[0, 5]}-#{value[5, 3]}"
+    when "status"
+      value.capitalize
+    else
+      value
+    end
+  end
 end
