a4565ce6a1e1e5f0ba79ce255d1654ab3dcf7f49
Elson Otake <elsonotake@yahoo.com.br>
2024-02-24 09:39:01 -0300

    Remove cities unused route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/cities'
   resources :boletos
   get "cities", controller: "home"
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "boletos#index"
 end
