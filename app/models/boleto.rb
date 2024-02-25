e59205feabc466e7d4b45faac17e6badbbedbd99
Elson Otake <elsonotake@yahoo.com.br>
2024-02-25 18:23:45 -0300

    Create persisted? action

--- app/models/boleto.rb
+++ app/models/boleto.rb
@@ -13,4 +13,22 @@ class Boleto
   attribute :customer_address, :string, default: nil
   attribute :customer_neighborhood, :string, default: nil
   attribute :status, :string, default: nil
+
+  def persisted?
+    puts "amount: #{self.amount} name: #{self.customer_person_name}"
+    bank_billet = BoletoSimples::BankBillet.create(
+      amount: self.amount,
+      expire_at: self.expire_at,
+      customer_person_name: self.customer_person_name,
+      customer_cnpj_cpf: self.customer_cnpj_cpf,
+      customer_state: self.customer_state,
+      customer_city_name: self.customer_city_name,
+      customer_zipcode: self.customer_zipcode,
+      customer_address: self.customer_address,
+      customer_neighborhood: self.customer_neighborhood
+    )
+    self.id = bank_billet.id
+    self.status = bank_billet.status
+    bank_billet.response_errors.empty?
+  end
 end
