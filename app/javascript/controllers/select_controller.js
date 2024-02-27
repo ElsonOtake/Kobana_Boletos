d24ce78227f1292796417a40aeafb77992bbe5aa
Elson Otake <elsonotake@yahoo.com.br>
2024-02-27 12:00:17 -0300

    Refactor js select

--- app/javascript/controllers/select_controller.js
+++ app/javascript/controllers/select_controller.js
@@ -4,24 +4,42 @@ export default class extends Controller {
   static targets = [ "state", "city" ];
   static values = { url: String }
 
-  load_option = (tag, values) => {
+  load_option = (values) => {
     values.forEach((value) => {
       const option = document.createElement("option");
       option.value = value;
       option.text = value;
-      tag.appendChild(option);
+      this.cityTarget.appendChild(option);
     });
   }
 
+  clear_options = () => {
+    let child = this.cityTarget.lastElementChild;
+    while (child) {
+      this.cityTarget.removeChild(child);
+      child = this.cityTarget.lastElementChild;
+    };
+  }
+
+  add_prompt = () => {
+    if (this.cityTarget.childElementCount == 0) {
+      const prompt = document.createElement("option");
+      prompt.innerText = "Selecione o estado";
+      this.cityTarget.appendChild(prompt);
+    }
+  }
+
   state_change = () => {
+    this.clear_options();
     if (this.stateTarget.value != "") {
-      this.cityTarget.length = 0;
       const url = new URL(this.urlValue)
       url.searchParams.append("state", this.stateTarget.value)
       fetch(url)
       .then(response => response.json())
-      .then((data) => this.load_option(this.cityTarget, data))
+      .then((data) => this.load_option(data))
       .catch(() => [])
-    }
+    } else {
+      this.add_prompt();
+    };
   }
 }
