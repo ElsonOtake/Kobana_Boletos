ef0efcc293637bcb98e023e76f9f0bec6fd7c30f
Elson Otake <elsonotake@yahoo.com.br>
2024-02-28 17:10:08 -0300

    Add controller tests

--- test/controllers/boletos_controller_test.rb
+++ test/controllers/boletos_controller_test.rb
@@ -1,6 +1,29 @@
 require "test_helper"
 
 class BoletosControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    # Criar boleto
+    params = {
+      amount: 132.99,
+      expire_at: Date.today + 15,
+      customer_person_name: "Museu do Amanhã",
+      customer_cnpj_cpf: "04.393.475/0004-99",
+      customer_state: "RJ",
+      customer_city_name: "Rio de Janeiro",
+      customer_zipcode: "20081240",
+      customer_address: "Praça Mauá, 1",
+      customer_neighborhood: "Centro"
+    }
+    @boleto = Boleto.new(params)
+    @boleto.create
+    assert @boleto.persisted?
+  end
+
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+
   test "should get index" do
     get boletos_url
     assert_response :success
@@ -8,6 +31,16 @@ class BoletosControllerTest < ActionDispatch::IntegrationTest
 
   test "should get create" do
     get new_boleto_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get boleto_url(@boleto)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_boleto_url(@boleto)
     assert_response :success
   end
 end
