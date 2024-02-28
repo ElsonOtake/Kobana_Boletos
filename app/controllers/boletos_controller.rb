ac1bd22e69223d8c82d018bf301e93b5c2411645
Elson Otake <elsonotake@yahoo.com.br>
2024-02-28 15:20:39 -0300

    Fix variable name typo

--- app/controllers/boletos_controller.rb
+++ app/controllers/boletos_controller.rb
@@ -52,7 +52,7 @@ class BoletosController < ApplicationController
     @boleto = Boleto.new.find(params[:id])
     unless @boleto.persisted?
       respond_to do |format|
-        message = JSON.parse(boleto.response_errors).first.with_indifferent_access[:title]
+        message = JSON.parse(@boleto.response_errors).first.with_indifferent_access[:title]
         format.html { redirect_to root_path, notice: message }
         format.turbo_stream { flash.now[:notice] = message }
       end
