2e1330e15f7eebfdd512561981ff8c969582a929
Elson Otake <elsonotake@yahoo.com.br>
2024-02-28 21:11:26 -0300

    Add controller tests

--- test/controllers/boletos_controller_test.rb
+++ test/controllers/boletos_controller_test.rb
@@ -19,28 +19,54 @@ class BoletosControllerTest < ActionDispatch::IntegrationTest
     assert @boleto.persisted?
   end
 
-  test "should get root" do
+  test "deve ir para raiz" do
     get root_url
     assert_response :success
   end
 
-  test "should get index" do
+  test "deve ir a lista de boletos" do
     get boletos_url
     assert_response :success
   end
 
-  test "should get create" do
+  test "deve ir para criação de boleto" do
     get new_boleto_url
     assert_response :success
   end
 
-  test "should get show" do
+  test "deve apresentar o boleto" do
     get boleto_url(@boleto)
     assert_response :success
   end
 
-  test "should get edit" do
+  test "deve chamar o cancelamento do boleto" do
     get edit_boleto_url(@boleto)
+    assert_response :success
+  end
+
+  test "deve cancelar o boleto" do
+    patch boleto_url(@boleto)
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+  end
+
+  test "deve criar um boleto" do
+    post boletos_url, params: {
+      boleto: {
+        amount: 132.99,
+        expire_at: Date.today + 15,
+        customer_person_name: "Museu do Amanhã",
+        customer_cnpj_cpf: "04.393.475/0004-99",
+        customer_state: "RJ",
+        customer_city_name: "Rio de Janeiro",
+        customer_zipcode: "20081240",
+        customer_address: "Praça Mauá, 1",
+        customer_neighborhood: "Centro"
+      }
+    }
+    assert_response :redirect
+    follow_redirect!
     assert_response :success
   end
 end
