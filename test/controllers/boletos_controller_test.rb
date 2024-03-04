16cd8db17a86e98803a6c3e0021c62117d2500d3
Elson Otake <elsonotake@yahoo.com.br>
2024-03-04 20:33:36 -0300

    Add I18n to controller tests

--- test/controllers/boletos_controller_test.rb
+++ test/controllers/boletos_controller_test.rb
@@ -2,7 +2,6 @@ require "test_helper"
 
 class BoletosControllerTest < ActionDispatch::IntegrationTest
   setup do
-    # Criar boleto
     params = {
       amount: 132.99,
       expire_at: Date.today + 15,
@@ -19,54 +18,64 @@ class BoletosControllerTest < ActionDispatch::IntegrationTest
     assert @boleto.persisted?
   end
 
-  test "deve ir para raiz" do
-    get root_url
-    assert_response :success
-  end
+  { default: nil, pt: :pt, en: :en }.each do |key, value|
 
-  test "deve ir a lista de boletos" do
-    get boletos_url
-    assert_response :success
-  end
+    # deve ir para raiz
+    test "should go to the root path for #{key.to_s} locale" do
+      get root_url(locale: value)
+      assert_response :success
+    end
 
-  test "deve ir para criação de boleto" do
-    get new_boleto_url
-    assert_response :success
-  end
+    # deve ir a lista de boletos
+    test "should present the bank billet list for #{key.to_s} locale" do
+      get boletos_url(locale: value)
+      assert_response :success
+    end
 
-  test "deve apresentar o boleto" do
-    get boleto_url(@boleto)
-    assert_response :success
-  end
+    # deve abrir o formulário para criar um boleto
+    test "should open the form to create a bank billet for #{key.to_s} locale" do
+      get new_boleto_url(locale: value)
+      assert_response :success
+    end
 
-  test "deve chamar o cancelamento do boleto" do
-    get edit_boleto_url(@boleto)
-    assert_response :success
-  end
+    # deve apresentar o boleto
+    test "should show the bank billet for #{key.to_s} locale" do
+      get boleto_url(@boleto, locale: value )
+      assert_response :success
+    end
 
-  test "deve cancelar o boleto" do
-    patch boleto_url(@boleto)
-    assert_response :redirect
-    follow_redirect!
-    assert_response :success
-  end
+    # deve confirmar o cancelamento do boleto
+    test "should confirm the bank billet cancel for #{key.to_s} locale" do
+      get edit_boleto_url(@boleto, locale: value )
+      assert_response :success
+    end
 
-  test "deve criar um boleto" do
-    post boletos_url, params: {
-      boleto: {
-        amount: 132.99,
-        expire_at: Date.today + 15,
-        customer_person_name: "Museu do Amanhã",
-        customer_cnpj_cpf: "04.393.475/0004-99",
-        customer_state: "RJ",
-        customer_city_name: "Rio de Janeiro",
-        customer_zipcode: "20081240",
-        customer_address: "Praça Mauá, 1",
-        customer_neighborhood: "Centro"
+    # deve cancelar o boleto
+    test "should cancel a bank billet for #{key.to_s} locale" do
+      patch boleto_url(@boleto, locale: value )
+      assert_response :redirect
+      follow_redirect!
+      assert_response :success
+    end
+
+    # deve criar um boleto
+    test "should create a bank billet for #{key.to_s} locale" do
+      post boletos_url(locale: value), params: {
+        boleto: {
+          amount: 132.99,
+          expire_at: Date.today + 15,
+          customer_person_name: "Museu do Amanhã",
+          customer_cnpj_cpf: "04.393.475/0004-99",
+          customer_state: "RJ",
+          customer_city_name: "Rio de Janeiro",
+          customer_zipcode: "20081240",
+          customer_address: "Praça Mauá, 1",
+          customer_neighborhood: "Centro"
+        }
       }
-    }
-    assert_response :redirect
-    follow_redirect!
-    assert_response :success
+      assert_response :redirect
+      follow_redirect!
+      assert_response :success
+    end
   end
 end
